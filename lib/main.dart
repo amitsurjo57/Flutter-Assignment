9c16409136ead30901fdee28cb8f6c87a9723aca
Amit Banik Surjo <amitsurjo57@gmail.com>
2024-08-01 23:15:26 +0600

    Greeting App

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(fontSize: 20),
+        ),
+      ),
+      home: const GreetingApp(),
+    );
+  }
+}
+
+class GreetingApp extends StatelessWidget {
+  const GreetingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Greeting App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Hello World!",
+              style: TextStyle(
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              "Welcome to Flutter!",
+            ),
+            Image.asset(
+              "assets/images/flutter.jpg",
+              width: 300,
+              height: 250,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text(
+                      "Button Pressed!",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+              child: const Text("Press me"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
