411702b183cee95a01c54c8ce8aaace7db9eaa6b
 <amitsurjo57@gmail.com>
2024-09-05 19:24:56 +0600

    My Bag

---
+++ lib/shirt.dart
@@ -0,0 +1,15 @@
+class Shirt {
+  String imagePath;
+  String shirtType;
+  String shirtColor;
+  String shirtSize;
+  int shirtPrice;
+
+  Shirt({
+    required this.imagePath,
+    required this.shirtType,
+    required this.shirtColor,
+    required this.shirtSize,
+    required this.shirtPrice,
+  });
+}
---
+++ lib/shirt_list.dart
@@ -0,0 +1,25 @@
+import 'shirt.dart';
+
+List<Shirt> shirtList = [
+  Shirt(
+    imagePath: 'assets/images/T shirt.png',
+    shirtType: 'Pullover',
+    shirtColor: 'Black',
+    shirtSize: 'L',
+    shirtPrice: 51,
+  ),
+  Shirt(
+    imagePath: 'assets/images/T shirt.png',
+    shirtType: 'T-Shirt',
+    shirtColor: 'Gray',
+    shirtSize: 'L',
+    shirtPrice: 30,
+  ),
+  Shirt(
+    imagePath: 'assets/images/flutter T shirt.png',
+    shirtType: 'Sport Dress',
+    shirtColor: 'Black',
+    shirtSize: 'M',
+    shirtPrice: 43,
+  ),
+];
